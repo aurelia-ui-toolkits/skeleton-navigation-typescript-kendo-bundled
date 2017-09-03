27fa9a144bf68925ee0c49762b05bb181e15873c
Nikolaj Ivancic <nik@congral.com>
2017-09-03 12:46:02 -0400

    nc

--- build/bundles.js
+++ build/bundles.js
@@ -1,64 +1,64 @@
 module.exports = {
-  "bundles": {
-    "dist/app-build": {
-      "includes": [
-        "[**/*.js]",
-        "**/*.html!text",
-        "**/*.css!text"
+  'bundles': {
+    'dist/app-build': {
+      'includes': [
+        '[**/*.js]',
+        '**/*.html!text',
+        '**/*.css!text'
       ],
-      "options": {
-        "inject": true,
-        "minify": true,
-        "depCache": true,
-        "rev": false
+      'options': {
+        'inject': true,
+        'minify': true,
+        'depCache': true,
+        'rev': false
       }
     },
-    "dist/plugins": {
-      "includes": [
-        "[aurelia-kendoui-bridge]",
-        "[aurelia-kendoui-bridge/**/*.js]",
-        "aurelia-kendoui-bridge/**/*.html!text",
+    'dist/plugins': {
+      'includes': [
+        '[aurelia-kendoui-bridge]',
+        '[aurelia-kendoui-bridge/**/*.js]',
+        'aurelia-kendoui-bridge/**/*.html!text'
       ],
-      "options": {
-        "inject": true,
-        "minify": true,
-        "depCache": false,
-        "rev": false
+      'options': {
+        'inject': true,
+        'minify': true,
+        'depCache': false,
+        'rev': false
       }
     },
-    "dist/aurelia": {
-      "includes": [
-        "aurelia-framework",
-        "aurelia-bootstrapper",
-        "aurelia-fetch-client",
-        "aurelia-router",
-        "aurelia-animator-css",
-        "aurelia-templating-binding",
-        "aurelia-polyfills",
-        "aurelia-templating-resources",
-        "aurelia-templating-router",
-        "aurelia-loader-default",
-        "aurelia-history-browser",
-        "aurelia-logging-console",
-        "bootstrap",
-        "bootstrap/css/bootstrap.css!text",
-        "fetch",
-        "jquery"
+    'dist/aurelia': {
+      'includes': [
+        'aurelia-framework',
+        'aurelia-bootstrapper',
+        'aurelia-fetch-client',
+        'aurelia-router',
+        'aurelia-animator-css',
+        'aurelia-templating-binding',
+        'aurelia-polyfills',
+        'aurelia-templating-resources',
+        'aurelia-templating-router',
+        'aurelia-loader-default',
+        'aurelia-history-browser',
+        'aurelia-logging-console',
+        'bootstrap',
+        'bootstrap/css/bootstrap.css!text',
+        'fetch',
+        'jquery'
       ],
-      "options": {
-        "inject": true,
-        "minify": true,
-        "depCache": false,
-        "rev": false
+      'options': {
+        'inject': true,
+        'minify': true,
+        'depCache': false,
+        'rev': false
       }
     },
-    "dist/kendo-build": {
-      "includes": ["kendo-ui/js/*.min.js"],
-      "excludes": ["kendo-ui/js/kendo.all.min.js"],
-      "options": {
-        "inject": true,
-        "minify": true,
-        "rev": true
+    'dist/kendo-build': {
+      'includes': ['kendo-ui/js/*.min.js'],
+      'excludes': ['kendo-ui/js/kendo.all.min.js'],
+      'options': {
+        'inject': true,
+        'minify': true,
+        'rev': true
       }
     }
   }
